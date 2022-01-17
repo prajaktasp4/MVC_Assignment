ec93366fb902acfe8a962173b912926c92eb265f
prajaktasp4 <95018607+prajaktasp4@users.noreply.github.com>
2022-01-17 23:35:34 +0530

    Add files via upload

---
+++ Practice1/AccountController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace First_app.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login(string username,string password)
+        {
+            if(username=="admin" && password=="admin@123")
+            {
+                return RedirectToAction("Dashboard", "Admin");
+            }
+            else
+            {
+                return RedirectToAction("InvalidLogin");
+            }
+           
+        }
+
+        public ActionResult InvalidLogin()
+        {
+            return View();
+        }
+    }
+}
---
+++ Practice1/HomeController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace First_app.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Product()
+        {
+            return View("CompanyProduct");
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public ActionResult GetEmpName(int EmpId)
+        {
+            var Emp = new[]
+            {
+                new  { EmpId =1,EmpName="Scot",Salary=20000 },
+                new { EmpId =2,EmpName="Smith",Salary=30000},
+                new  { EmpId =3,EmpName="Allen",Salary=50000}
+            };
+            string MatchEmpName = null;
+
+            foreach(var item in Emp)
+            {
+                if(item.EmpId == EmpId)
+                {
+                    MatchEmpName= item.EmpName;
+                }
+
+            }
+            return Content(MatchEmpName,"plain/text");
+        }
+
+        public ActionResult GetPaySlip(int EmpId)
+        {
+            string FileName = "~/PaySlip" + EmpId + ".pdf";
+            return File(FileName, "application/pdf");
+
+
+        }
+
+        public ActionResult EmpFacebookPage(int EmpId)
+        {
+            var Emp = new[]
+            {
+                new  { EmpId =1,EmpName="Scot",Salary=20000 },
+                new { EmpId =2,EmpName="Smith",Salary=30000},
+                new  { EmpId =3,EmpName="Allen",Salary=50000}
+            };
+            string fburl = null;
+            foreach (var item in Emp)
+            {
+                if(item.EmpId==EmpId)
+                {
+                    fburl = "http://www.facebook.com.emp" + EmpId;
+                }
+            }
+            if(fburl != null)
+            {
+                return Content("Invalid Employee id");
+            }
+            else
+            {
+                return Redirect(fburl);
+            }
+
+           
+        }
+
+
+    }
+}
