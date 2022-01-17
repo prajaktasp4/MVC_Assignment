73d9c70b9fde08e7b46076a531d99bc02e5716dc
prajaktasp4 <95018607+prajaktasp4@users.noreply.github.com>
2022-01-17 23:30:17 +0530

    Add files via upload

---
+++ Assignmentno_1/NewHomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace First_app.Controllers
+{
+    public class NewHomeController : Controller
+    {
+        // GET: NewHome
+        public ActionResult Index(string str)
+        {
+           
+
+                if (str == "Sample")
+                {
+                    String filepath = "~/Sample.pdf";
+                    return File(filepath, "application/pdf");
+                }
+                else if (str == "gotoabout")
+                {
+                    return RedirectToAction("About");
+                }
+
+                else if (str == "login")
+                {
+                    return View();
+
+                }
+
+                else
+                {
+                    return Content("You Entered :" + str);
+                }
+            
+            return View();
+
+        }
+        public ActionResult About()
+        {
+            return Content("About content here");
+        }
+    }
+}
