4f54ab30e76d8100f2e6f211e88f360f1ad69328
prajaktasp4 <95018607+prajaktasp4@users.noreply.github.com>
2022-01-18 22:17:30 +0530

    Add files via upload

---
+++ Assignment2/AmountController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace First_app.Controllers
+{
+    public class AmountController : Controller
+    {
+        // GET: Amount
+        public ActionResult Currency(int amt)
+        {
+            ViewBag.Amount = amt;
+            return View();
+        }
+    }
+}
