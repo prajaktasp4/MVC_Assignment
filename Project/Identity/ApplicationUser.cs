e349eb12657290b5bd7e3b16a41bbeea14ea4197
prajaktasp4 <95018607+prajaktasp4@users.noreply.github.com>
2022-02-02 20:24:53 +0530

    Add files via upload

---
+++ Project/Identity/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security.Cookies;
+
+namespace StudentManagmentSystem
+{
+    public class ApplicationUser:IdentityUser
+    {
+        public string Address { get; set; }
+        public string City { get; set; }
+    }
+}
